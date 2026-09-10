1452fa271b3260ba0d4fe7cde7c9ea665c1c71b2
Truong Anh Khoi <anhkhoi.truong@grenoble-inp.org>
2026-09-10 15:24:27 +0200

    error handling for local docker connection and oci connection

--- src/access.rs
+++ src/access.rs
@@ -5,17 +5,44 @@ use bollard::query_parameters::ListImagesOptionsBuilder;
 use chrono_tz::Tz;
 use oci_client::secrets::RegistryAuth;
 use oci_client::{ParseError, Reference};
+use std::fmt;
+
+#[derive(Debug)]
+pub enum AccessError {
+    DockerError(bollard::errors::Error),
+    OciError(ParseError),
+}
+
+impl fmt::Display for AccessError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            // Both underlying errors already impl `Display`, so we defer to
+            // their implementations.
+            AccessError::DockerError(ref err) => write!(f, "Docker connectin error: {}", err),
+            AccessError::OciError(ref err) => write!(f, "Remote repo error: {}", err),
+        }
+    }
+}
+
+impl From<bollard::errors::Error> for AccessError {
+    fn from(err: bollard::errors::Error) -> AccessError {
+        AccessError::DockerError(err)
+    }
+}
+
+impl From<ParseError> for AccessError {
+    fn from(err: ParseError) -> AccessError {
+        AccessError::OciError(err)
+    }
+}
 
 pub async fn compare_all_digest(
     tz: &Tz,
     maybe_discord_config: &Option<DiscordConfig>,
-) -> Result<(), String> {
-    let docker = Docker::connect_with_local_defaults().map_err(|e| e.to_string())?;
+) -> Result<(), AccessError> {
+    let docker = Docker::connect_with_local_defaults()?;
     let options = ListImagesOptionsBuilder::default().digests(true).build();
-    let images = &docker
-        .list_images(Some(options))
-        .await
-        .map_err(|e| e.to_string())?;
+    let images = &docker.list_images(Some(options)).await?;
 
     let client = oci_client::Client::default();
     for image in images {
@@ -23,9 +50,8 @@ pub async fn compare_all_digest(
         if !tags_list.is_empty() {
             let tag = &tags_list[0];
             let repo_digest_list = &image.repo_digests;
-            let repo_digest = &repo_digest_list[0]; //need to think about the case that
-            //repo_digests is empty, normally wont happen
-            let reference: &Reference = &tag.parse().map_err(|e: ParseError| e.to_string())?; //error here need to handle
+            let repo_digest = &repo_digest_list[0]; //need to think about the case that repo_digests is empty, normally wont happen
+            let reference: &Reference = &tag.parse()?; //error here need to handle
 
             let remote_digest = match oci_client::Client::fetch_manifest_digest(
                 &client,
